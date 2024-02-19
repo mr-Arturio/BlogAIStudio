f1a24f9950faeaf30d033dbb747fd802b9986638
ArturT <mr.arturio@gmail.com>
2024-02-19 13:25:03 -0500

    initialize pages and routs

---
+++ pages/post/new.js
@@ -0,0 +1,7 @@
+export default function NewPost() {
+  return (
+    <div>
+      <h1>This is the New post page</h1>
+    </div>
+  );
+}
---
+++ pages/token-topup.js
@@ -0,0 +1,7 @@
+export default function TokenTopup() {
+  return (
+    <div>
+      <h1>This is the Token Popup page</h1>
+    </div>
+  );
+}
