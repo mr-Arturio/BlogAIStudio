40330b28f35bcb669810f660c273193fa87312a8
ArturT <mr.arturio@gmail.com>
2024-02-20 23:42:39 -0500

    add basic styling with Tailwind

--- components/AppLayout/AppLayout.js
+++ components/AppLayout/AppLayout.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React from "react";
 
-export const AppLayout = ({children}) => {
+export const AppLayout = ({ children }) => {
   return (
-    <div>This is the App Layout<div>{children}</div></div>
-  )
-}
+    <div className="grid grid-cols-[300px_1fr] h-screen max-h-screen">
+      <div className="flex flex-col text-white overflow-hidden">
+        <div className="bg-slate-800">
+          <div>logo</div>
+          <div>cta button</div>
+          <div>tokens</div>
+        </div>
+        <div className='flex-1 overflow-auto bg-gradient-to-b from-slate-800 to-cyan-800'>list of posts</div>
+        <div className="bg-cyan-800">user info - logout button</div>
+      </div>
+      <div className="bg-yellow-400"> {children}</div>
+    </div>
+  );
+};
