4e54cdf21f622c30fbc3d819580ace5106916a3b
ArturT <mr.arturio@gmail.com>
2024-02-21 22:57:30 -0500

    implement footer

--- components/AppLayout/AppLayout.js
+++ components/AppLayout/AppLayout.js
@@ -1,6 +1,11 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { useUser } from "@auth0/nextjs-auth0/client";
 
 export const AppLayout = ({ children }) => {
+  const { user, error, isLoading } = useUser();
+
   return (
     <div className="grid grid-cols-[300px_1fr] h-screen max-h-screen">
       <div className="flex flex-col text-white overflow-hidden">
@@ -9,10 +14,35 @@ export const AppLayout = ({ children }) => {
           <div>cta button</div>
           <div>tokens</div>
         </div>
-        <div className='flex-1 overflow-auto bg-gradient-to-b from-slate-800 to-cyan-800'>list of posts</div>
-        <div className="bg-cyan-800">user info - logout button</div>
+        <div className="flex-1 overflow-auto bg-gradient-to-b from-slate-800 to-cyan-800">
+          list of posts
+        </div>
+        <div className="bg-cyan-800 flex items-center gap-2 border-t border-t-black/50 h-20 px-2">
+          {" "}
+          {!!user ? (
+            <>
+              <div className="min-w-[50px]">
+                <Image
+                  src={user.picture}
+                  alt={user.name}
+                  height={50}
+                  width={50}
+                  className="rounded-full"
+                />
+              </div>
+              <div className="flex-1">
+                <div className="font-bold">{user.email}</div>{" "}
+                <Link className="text-sm" href="/api/auth/logout">
+                  Logout
+                </Link>
+              </div>
+            </>
+          ) : (
+            <Link href="/api/auth/login">Login</Link>
+          )}
+        </div>
       </div>
-      <div className="bg-yellow-400"> {children}</div>
+      <div className=""> {children}</div>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,10 @@
-import Link from "next/link";
-import Image from "next/image";
-import { useUser } from "@auth0/nextjs-auth0/client";
-
 export default function Home() {
-  const { user, error, isLoading } = useUser();
-
-  console.log("USER: ", user);
 
   return (
     <div>
       <h1>Home</h1>
       <div>
-        {!!user ? (
-          <>
-            <div>
-              <Image
-                src={user.picture}
-                alt={user.name}
-                height={50}
-                width={50}
-              />
-            </div>
-            <div>{user.email}</div>
-            <Link href="/api/auth/logout">Logout</Link>
-          </>
-        ) : (
-          <Link href="/api/auth/login">Login</Link>
-        )}
+       
       </div>
     </div>
   );
