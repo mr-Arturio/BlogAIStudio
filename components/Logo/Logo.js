e5e83b353ee2a4a3c6c36772e1900560d6426071
ArturT <mr.arturio@gmail.com>
2024-02-22 22:31:42 -0500

    logo files

---
+++ components/Logo/Logo.js
@@ -0,0 +1,11 @@
+import { faRobot } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+export const Logo = () => {
+  return (
+    <div className="text-3xl text-center py-4 font-heading">
+      BlogStandard
+      <FontAwesomeIcon icon={faRobot} className="text-2xl text-slate-400" />
+    </div>
+  );
+};
