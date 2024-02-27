ec2fa884540b7f5a86450641c1abdf1a57f7d883
ArturT <mr.arturio@gmail.com>
2024-02-27 14:18:20 -0500

    load the post data from Db for a particular post page

--- README.md
+++ README.md
@@ -11,6 +11,16 @@ OpenAI model: gpt-4-1106-preview
 MongoDb:
 - name - BlogAiStudio
 - tables - posts, users
+post content:
+- _id
+- postContent
+- title
+- metaDescription
+- topic
+- keywords
+- userId
+- created
+
 
 
 `openssl rand -hex 32` - command to generate a random string
--- pages/post/[postId].js
+++ pages/post/[postId].js
@@ -1,7 +1,12 @@
-import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { getSession, withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { AppLayout } from "../../components/AppLayout";
+import clientPromise from "../../lib/mongodb";
+import { ObjectId } from "mongodb";
 
+export default function Post(props) {
 
-export default function Post() {
+  console.log("PROPS: ", props);
+
   return (
     <div>
       <h1>This is the Post page</h1>
@@ -9,9 +14,48 @@ export default function Post() {
   );
 }
 
-export const getServerSideProps = withPageAuthRequired(() => {
-  //withPageAuthRequired is a higher-order function that requires authentication to access the page. If the user is not authenticated, they are redirected to the login page.
-  return {
-    props: {},
-  };
+Post.getLayout = function getLayout(page, pageProps) {
+  return <AppLayout {...pageProps}>{page}</AppLayout>;
+};
+
+//withPageAuthRequired is a higher-order function that requires authentication to access the page. If the user is not authenticated, they are redirected to the login page.
+export const getServerSideProps = withPageAuthRequired({
+  //getServerSideProps: fetch data server-side and pass it as props to the page. It runs on every request to the page in '/post/new' and it's called before the page component is rendered.
+  async getServerSideProps(ctx) {
+    // const props = await getAppProps(ctx); //context
+    const userSession = await getSession(ctx.req, ctx.res);
+    const client = await clientPromise;
+    const db = client.db("BlogAiStudio");
+    //grab posst by id
+    const user = await db.collection("users").findOne({
+      auth0Id: userSession.user.sub,
+    });
+    const post = await db.collection("posts").findOne({
+      _id: new ObjectId(ctx.params.postId),
+      userId: user._id, //grab post by user id that loged in
+    });
+
+    if (!post) {
+      //if post does not exist
+      return {
+        redirect: {
+          destination: "/post/new",
+          permanent: false,
+        },
+      };
+    }
+
+    return {
+      // if post exist return the data
+      props: {
+        // id: ctx.params.postId,
+        postContent: post.postContent,
+        title: post.title,
+        metaDescription: post.metaDescription,
+        keywords: post.keywords,
+        // postCreated: post.created.toString(),
+        // ...props,
+      },
+    };
+  },
 });
