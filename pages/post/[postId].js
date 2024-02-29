a20ce2da189b482c53bb149c4b777e713a151822
ArturT <mr.arturio@gmail.com>
2024-02-29 00:31:37 -0500

    import getAppProps and small edits

--- pages/post/[postId].js
+++ pages/post/[postId].js
@@ -5,6 +5,7 @@ import { ObjectId } from "mongodb";
 import Markdown from "react-markdown";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHashtag } from '@fortawesome/free-solid-svg-icons';
+import { getAppProps } from "../../utils/getAppProps";
 
 export default function Post(props) {
   console.log("PROPS: ", props);
@@ -33,56 +34,7 @@ export default function Post(props) {
           Blog post
         </div>
         <Markdown>{props.postContent || ""}</Markdown>
-        {/* <div className="p-4 my-2 border border-stone-200 rounded-md">
-          <div className="text-blue-600 text-2xl font-bold">{props.title}</div>
-          <div className="mt-2">{props.metaDescription}</div>
-        </div>
-        <div className="text-sm font-bold mt-6 p-2 bg-stone-200 rounded-sm">
-          Keywords
-        </div>
-        <div className="flex flex-wrap pt-2 gap-1">
-          {props.keywords.split(",").map((keyword, i) => (
-            <div key={i} className="p-2 rounded-full bg-slate-800 text-white">
-              <FontAwesomeIcon icon={faHashtag} /> {keyword}
-            </div>
-          ))}
-        </div>
-        <div className="text-sm font-bold mt-6 p-2 bg-stone-200 rounded-sm">
-          Blog post
-        </div>
-        <div dangerouslySetInnerHTML={{ __html: props.postContent || "" }} />
-        <div className="my-4">
-          {!showDeleteConfirm && (
-            <button
-              className="btn bg-red-600 hover:bg-red-700"
-              onClick={() => setShowDeleteConfirm(true)}
-            >
-              Delete post
-            </button>
-          )}
-          {!!showDeleteConfirm && (
-            <div>
-              <p className="p-2 bg-red-300 text-center">
-                Are you sure you want to delete this post? This action is
-                irreversible
-              </p>
-              <div className="grid grid-cols-2 gap-2">
-                <button
-                  onClick={() => setShowDeleteConfirm(false)}
-                  className="btn bg-stone-600 hover:bg-stone-700"
-                >
-                  cancel
-                </button>
-                <button
-                  onClick={handleDeleteConfirm}
-                  className="btn bg-red-600 hover:bg-red-700"
-                >
-                  confirm delete
-                </button>
-              </div>
-            </div>
-          )}
-        </div> */}
+
       </div>
     </div>
   );
@@ -96,7 +48,7 @@ Post.getLayout = function getLayout(page, pageProps) {
 export const getServerSideProps = withPageAuthRequired({
   //getServerSideProps: fetch data server-side and pass it as props to the page. It runs on every request to the page in '/post/new' and it's called before the page component is rendered.
   async getServerSideProps(ctx) {
-    // const props = await getAppProps(ctx); //context
+    const props = await getAppProps(ctx);
     const userSession = await getSession(ctx.req, ctx.res);
     const client = await clientPromise;
     const db = client.db("BlogAiStudio");
@@ -128,7 +80,7 @@ export const getServerSideProps = withPageAuthRequired({
         metaDescription: post.metaDescription,
         keywords: post.keywords,
         // postCreated: post.created.toString(),
-        // ...props,
+        ...props,
       },
     };
   },
