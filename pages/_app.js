efb56fe25cba3b81cc33822cc06697a96fbac19a
ArturT <mr.arturio@gmail.com>
2024-02-29 22:49:57 -0500

    create page for success payment

--- pages/_app.js
+++ pages/_app.js
@@ -4,26 +4,28 @@ import { DM_Sans, DM_Serif_Display } from "@next/font/google";
 
 //font-family specs
 const dmSans = DM_Sans({
-  weight: ['400', '500', '700'],
-  subsets: ['latin'],
-  variable: '--font-dm-sans',
+  weight: ["400", "500", "700"],
+  subsets: ["latin"],
+  variable: "--font-dm-sans",
 });
 
 const dmSerifDisplay = DM_Serif_Display({
-  weight: ['400'],
-  subsets: ['latin'],
-  variable: '--font-dm-serif',
+  weight: ["400"],
+  subsets: ["latin"],
+  variable: "--font-dm-serif",
 });
 
 function MyApp({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page);
   return (
-  <UserProvider>
-    <main className={`${dmSans.variable} ${dmSerifDisplay.variable} font-body`}>
-    {getLayout(<Component {...pageProps} />, pageProps)}
-    </main>
-  </UserProvider>
-  )
+    <UserProvider>
+      <main
+        className={`${dmSans.variable} ${dmSerifDisplay.variable} font-body`}
+      >
+        {getLayout(<Component {...pageProps} />, pageProps)}
+      </main>
+    </UserProvider>
+  );
 }
 
 export default MyApp;
--- pages/api/addTokens.js
+++ pages/api/addTokens.js
@@ -1,8 +1,40 @@
 import { getSession } from "@auth0/nextjs-auth0";
 import clientPromise from "../../lib/mongodb";
+import stripeInit from "stripe";
+
+//stripe API
+const stripe = stripeInit(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
   const { user } = await getSession(req, res);
+
+  // Create a PaymentIntent with the order amount
+  const lineItems = [
+    {
+      price: process.env.STRIPE_PRODUCT_PRICE_ID,
+      quantity: 1,
+    },
+  ];
+
+  //assigning protocol depends if we on local host or on the server
+  const protocol =
+    process.env.NODE_ENV === "development" ? "http://" : "https://";
+  const host = req.headers.host;
+
+  // Create a new Checkout Session using the builder pattern
+  const checkoutSession = await stripe.checkout.sessions.create({
+    line_items: lineItems,
+    mode: "payment",
+    success_url: `${protocol}${host}/success`,
+    // payment_intent_data: {
+    //   metadata: {
+    //     sub: user.sub,
+    //   },
+    // },
+    // metadata: {
+    //   sub: user.sub,
+    // },
+  });
 
   console.log("user: ", user);
 
@@ -27,5 +59,5 @@ export default async function handler(req, res) {
     }
   );
 
-  res.status(200).json({ name: "John Doe" });
+  res.status(200).json({ session: checkoutSession });
 }
