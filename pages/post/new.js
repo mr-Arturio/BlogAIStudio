f90d346917827111dc972ad30977d69c7d8c2877
ArturT <mr.arturio@gmail.com>
2024-03-03 00:41:12 -0500

    add styling and loading screen

--- pages/post/new.js
+++ pages/post/new.js
@@ -4,61 +4,85 @@ import { useState } from "react";
 import Markdown from "react-markdown";
 import { useRouter } from "next/router";
 import { getAppProps } from "../../utils/getAppProps";
+import { faRobot } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function NewPost(props) {
   const router = useRouter();
   const [topic, setTopic] = useState("");
   const [keywords, setKeywords] = useState("");
+  const [loading, setLoading] = useState(false);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch(`/api/generatePost`, {
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify({ topic, keywords }),
-    });
-    const data = await response.json();
+    setLoading(true);
+    try {
+      const response = await fetch(`/api/generatePost`, {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({ topic, keywords }),
+      });
+      const data = await response.json();
 
-    console.log("DATA: ", data);
+      console.log("DATA: ", data);
 
-    //check if postId exist and was generated successfully
-    if (data?.postId) {
-      //redirect to the post page
-      router.push(`/post/${data.postId}`);
+      //check if postId exist and was generated successfully
+      if (data?.postId) {
+        //redirect to the post page
+        router.push(`/post/${data.postId}`);
+      }
+    } catch (e) {
+      setLoading(false);
     }
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>
-            <strong>Generate a blog post on the topic of:</strong>
-          </label>
-          <textarea
-            className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm"
-            value={topic}
-            onChange={(e) => setTopic(e.target.value)}
-            maxLength={80}
-          />
+    <div className="h-full overflow-hidden">
+      {!!loading && (
+        <div className="text-green-500 flex h-full animate-pulse w-full flex-col justify-center items-center">
+          <FontAwesomeIcon icon={faRobot} className="text-8xl" />
+          <h6>Crafting...</h6>
         </div>
-        <div>
-          <label>
-            <strong>Targeting the following keywords:</strong>
-          </label>
-          <textarea
-            className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm"
-            value={keywords}
-            onChange={(e) => setKeywords(e.target.value)}
-            maxLength={80}
-          />
-          <small className="block mb-2">Separate keywords with a comma</small>{" "}
+      )}
+      {!loading && (
+        <div className="w-full h-full flex flex-col overflow-auto">
+          <form
+            onSubmit={handleSubmit}
+            className="m-auto w-full max-w-screen-sm bg-slate-100 p-4 rounded-md shadow-xl border border-slate-200 shadow-slate-200"
+          >
+            <div>
+              <label>
+                <strong>Generate a blog post on the topic of:</strong>
+              </label>
+              <textarea
+                className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm"
+                value={topic}
+                onChange={(e) => setTopic(e.target.value)}
+                maxLength={80}
+              />
+            </div>
+            <div>
+              <label>
+                <strong>Targeting the following keywords:</strong>
+              </label>
+              <textarea
+                className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm"
+                value={keywords}
+                onChange={(e) => setKeywords(e.target.value)}
+                maxLength={80}
+              />
+              <small className="block mb-2">
+                Separate keywords with a comma
+              </small>{" "}
+            </div>
+            <button type="submit" className="btn">
+              Generate
+            </button>{" "}
+          </form>
         </div>
-        <button type="submit" className="btn">
-          Generate
-        </button>{" "}
-      </form>
+      )}
     </div>
   );
 }
